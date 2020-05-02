52c7a62ababcdc4ff8ab56dcaee7df6d6c0a7c32
Travis Carr <tmcarr89@gmail.com>
2020-05-02 15:15:46 -0400

    I can add user descriptions now....

--- src/commands/desc.rs
+++ src/commands/desc.rs
@@ -1,27 +1,27 @@
-// use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
-use serenity::framework::standard::{macros::command, CommandResult};
+use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
+use serenity::framework::standard::{
+  Args, macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
-
-
-// let mut db = PickleDb::new(
-//   "testbot.db",
-//   PickleDbDumpPolicy::AutoDump,
-//   SerializationMethod::Json,
-// );
-
-
 // Command to write to DB
 #[command]
-fn describe(ctx: &mut Context, msg: &Message) -> CommandResult {
-  let _ = msg.channel_id.say(&ctx.http, "Unimplemented!");
+fn describe(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
+  let mut db = PickleDb::new(
+    "testbot.db",
+    PickleDbDumpPolicy::AutoDump,
+    SerializationMethod::Json,
+  );
+
+  let value = &args.parse::<String>().unwrap();
+  db.set(&String::from(&msg.author.name), value).unwrap();
+  let _ = msg.channel_id.say(&ctx.http, &format!("Set {}'s description to: '{}'", &msg.author.name, value));
   Ok(())
 }
 
 // Command to read from DB
 #[command]
-fn about(ctx: &mut Context, msg: &Message) -> CommandResult {
+fn about(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
   let _ = msg.channel_id.say(&ctx.http, "Unimplemented!");
 
   Ok(())
--- src/main.rs
+++ src/main.rs
@@ -10,88 +10,93 @@
 //! ```
 mod commands;
 
-use std::{
-    collections::HashSet,
-    env,
-    sync::Arc,
-};
-use serenity::{
-    client::bridge::gateway::ShardManager,
-    framework::{
-        StandardFramework,
-        standard::macros::group,
-    },
-    model::{event::ResumedEvent, gateway::Ready},
-    prelude::*,
-};
 use log::{error, info};
+use serenity::{
+  client::bridge::gateway::ShardManager,
+  framework::standard::{
+    macros::group, StandardFramework,
+  },
+  model::{event::ResumedEvent, gateway::Ready},
+  prelude::*,
+};
+use std::{collections::HashSet, env, sync::Arc};
 
 use commands::{
-    math::*,
-    meta::*,
-    owner::*,
-    desc::*,
+  math::*,
+  meta::*,
+  owner::*,
+  desc::*,
 };
 struct ShardManagerContainer;
 
 impl TypeMapKey for ShardManagerContainer {
-    type Value = Arc<Mutex<ShardManager>>;
+  type Value = Arc<Mutex<ShardManager>>;
 }
 
 struct Handler;
 
 impl EventHandler for Handler {
-    fn ready(&self, _: Context, ready: Ready) {
-        info!("Connected as {}", ready.user.name);
-    }
+  fn ready(&self, _: Context, ready: Ready) {
+    info!("Connected as {}", ready.user.name);
+  }
 
-    fn resume(&self, _: Context, _: ResumedEvent) {
-        info!("Resumed");
-    }
+  fn resume(&self, _: Context, _: ResumedEvent) {
+    info!("Resumed");
+  }
 }
 
 #[group]
-#[commands(add, multiply, ping, quit)]
+#[commands(describe, about, add, multiply, ping, quit)]
 struct General;
 
 fn main() {
-    // This will load the environment variables located at `./.env`, relative to
-    // the CWD. See `./.env.example` for an example on how to structure this.
-    kankyo::load().expect("Failed to load .env file");
+  // This will load the environment variables located at `./.env`, relative to
+  // the CWD. See `./.env.example` for an example on how to structure this.
+  kankyo::load().expect("Failed to load .env file");
 
-    // Initialize the logger to use environment variables.
-    //
-    // In this case, a good default is setting the environment variable
-    // `RUST_LOG` to debug`.
-    env_logger::init();
+  // Initialize the logger to use environment variables.
+  //
+  // In this case, a good default is setting the environment variable
+  // `RUST_LOG` to debug`.
+  env_logger::init();
 
-    let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment");
+  let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-    let mut client = Client::new(&token, Handler).expect("Err creating client");
+  let mut client = Client::new(&token, Handler).expect("Err creating client");
 
-    {
-        let mut data = client.data.write();
-        data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+  {
+    let mut data = client.data.write();
+    data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+  }
+
+  let (owners, bot_id) = match client.cache_and_http.http.get_current_application_info() {
+    Ok(info) => {
+      let mut owners = HashSet::new();
+      owners.insert(info.owner.id);
+
+      (owners, info.id)
     }
+    Err(why) => panic!("Couldn't get application info: {:?}", why),
+  };
 
-    let owners = match client.cache_and_http.http.get_current_application_info() {
-        Ok(info) => {
-            let mut set = HashSet::new();
-            set.insert(info.owner.id);
+  client.with_framework(
+    StandardFramework::new()
+      .configure(|c| c
+        .on_mention(Some(bot_id))
+        .owners(owners)
+        .prefix("~"))
+      .before(|ctx, msg, command_name| {
+        println!(
+          "Got command '{}' by user '{}': '{}'",
+          command_name, msg.author, msg.content,
+        );
 
-            set
-        },
-        Err(why) => panic!("Couldn't get application info: {:?}", why),
-    };
+        true // if `before` returns false, command processing doesn't happen.
+      })
+      .group(&GENERAL_GROUP),
+  );
 
-    client.with_framework(StandardFramework::new()
-        .configure(|c| c
-            .owners(owners)
-            .prefix("~"))
-        .group(&GENERAL_GROUP));
-
-    if let Err(why) = client.start() {
-        error!("Client error: {:?}", why);
-    }
+  if let Err(why) = client.start() {
+    error!("Client error: {:?}", why);
+  }
 }
