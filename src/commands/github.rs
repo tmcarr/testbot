ca7c15b1b894052659bb8778fb860ec638752a10
Travis Carr <tmcarr89@gmail.com>
2020-07-12 16:39:49 -0400

    fix imports, fix URL, and clean up new command...

--- src/commands/github.rs
+++ src/commands/github.rs
@@ -1,12 +1,11 @@
-https://github.com/tmcarr/testbot
-
+use serenity::framework::standard::{macros::command, CommandResult};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
 
 #[command]
 #[aliases("source")]
-fn github(ctx: &mut Context, msg: &Message, args: Args) -> CommandResult {
-    let value = &args.message();
-    db.set(&String::from(&msg.author.name), value).unwrap();
-    let github = "'https://gihub.con/tmcarr/testbot";
+fn github(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let github = "'https://gihub.com/tmcarr/testbot";
     let _ = msg.channel_id.say(
         &ctx.http,
         &format!("My code is at: {}", &github),
