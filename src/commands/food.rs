7aee4cee7d846ee9f5b33a68913b7bcb75637628
Travis Carr <tmcarr89@gmail.com>
2020-09-02 20:02:53 -0400

    Add a food command

---
+++ src/commands/food.rs
@@ -0,0 +1,29 @@
+use rand::seq::SliceRandom;
+use serenity::framework::standard::{macros::command, CommandResult};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+
+#[command]
+#[aliases("food", "dinner", "lunch", "breakfast", "snack")]
+fn food(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let responses = vec![
+        "Asian",
+        "Barbecue",
+        "Hamburgers",
+        "Italian",
+        "Mexican",
+        "Pho",
+        "Pizza",
+        "Steak",
+        "Seafood",
+        "Indian",
+        "Cajun",
+    ];
+
+    let _ = msg.channel_id.say(
+        &ctx.http,
+        responses.choose(&mut rand::thread_rng()).unwrap(),
+    );
+
+    Ok(())
+}
