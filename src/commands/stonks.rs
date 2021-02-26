9c5f99008352d053d1275a6d30121649366cceeb
Travis Carr <tmcarr89@gmail.com>
2021-02-25 22:14:55 -0500

    Initial work on price function, unignore cargo.lock

--- src/commands/stonks.rs
+++ src/commands/stonks.rs
@@ -19,3 +19,14 @@ async fn stonks(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     }
     Ok(())
 }
+
+#[command]
+#[aliases("sprice", "stonkprice", "stockprice")]
+async fn price(ctx: &Context, msg: &Message) -> CommandResult {
+    let endpoint = "https://api.adviceslip.com/advice";
+    let result_text = reqwest::get(endpoint).await?.json::<Advice>().await?;
+    let results = format!("{} - #{}", result_text.slip.advice, result_text.slip.id);
+
+    let _ = msg.channel_id.say(&ctx.http, results).await;
+    Ok(())
+}
