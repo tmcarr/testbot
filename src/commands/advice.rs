5de0425b31b1e18ba5d10af8c0b47489cd671e8d
Travis Carr <tmcarr89@gmail.com>
2021-02-25 23:34:49 -0500

    Const where relevant

--- src/commands/advice.rs
+++ src/commands/advice.rs
@@ -16,8 +16,8 @@ struct Advice {
 
 #[command]
 async fn advice(ctx: &Context, msg: &Message) -> CommandResult {
-    let endpoint = "https://api.adviceslip.com/advice";
-    let result_text = reqwest::get(endpoint).await?.json::<Advice>().await?;
+    const endpoint: String = "https://api.adviceslip.com/advice";
+    let result_text = reqwest::get(&endpoint).await?.json::<Advice>().await?;
     let results = format!("{} - #{}", result_text.slip.advice, result_text.slip.id);
 
     let _ = msg.channel_id.say(&ctx.http, results).await;
